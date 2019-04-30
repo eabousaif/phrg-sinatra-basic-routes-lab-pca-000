275682114524dbc9424fa46247d820e5b3269104
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2019-04-30 11:13:23 -0400

    Basic routes in Sinatra

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Ezz Abousaif"
+  end
+
+  get '/hometown' do
+    "My hometown is Alexandria, Egypt"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is m.A.A.d City by Kendrick Lamar"
+  end
+
 end
